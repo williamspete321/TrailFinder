b3edecbfe0aede800dd57679a2bde6a969df47ff
Gerard <williamspete321@gmail.com>
2020-05-09 22:13:43 -0400

    style: improves TrailDetailFragment's code style

--- app/src/main/java/com/example/android/trailfinder/ui/traildetail/TrailDetailFragment.java
+++ app/src/main/java/com/example/android/trailfinder/ui/traildetail/TrailDetailFragment.java
@@ -3,24 +3,18 @@ package com.example.android.trailfinder.ui.traildetail;
 import android.location.Location;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
 
 import com.example.android.trailfinder.R;
 import com.example.android.trailfinder.databinding.FragmentTrailDetailBinding;
-import com.example.android.trailfinder.data.database.model.Trail;
 import com.example.android.trailfinder.utilities.InjectorUtils;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -31,7 +25,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import java.util.Locale;
 
@@ -42,22 +35,13 @@ public class TrailDetailFragment extends Fragment implements OnMapReadyCallback 
     private FragmentTrailDetailBinding binding;
 
     public static final String ID = "trail-id";
-
     private int trailId;
 
-    private FusedLocationProviderClient fusedLocationClient;
-    private Location userLocation;
     private SupportMapFragment mapFragment;
     private GoogleMap map;
 
-    private AppBarLayout appBar;
     private NestedScrollView nestedScrollView;
-    private ViewGroup rootLayout;
     private boolean toolbarLocked = false;
-
-    private TrailDetailViewModel viewModel;
-    private TrailDetailViewModelFactory factory;
-    private Trail currentTrail;
 
     public TrailDetailFragment() {
     }
@@ -72,49 +56,48 @@ public class TrailDetailFragment extends Fragment implements OnMapReadyCallback 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        if(getArguments() != null) {
+            trailId = getArguments().getInt(ID);
+        }
+
         binding = FragmentTrailDetailBinding.inflate(inflater, container, false);
-        View view = binding.getRoot();
+        nestedScrollView = binding.trailDetailScrollingLayout.nestedScrollView;
 
         ((AppCompatActivity) getActivity()).setSupportActionBar(binding.fragmentDetailToolbar);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        if(getArguments() != null) {
-            trailId = getArguments().getInt(ID);
-        }
-
+        enableToolbarLock();
+        setupMap();
         getLastLocation();
 
-        return view;
+        return binding.getRoot();
     }
 
     private void getLastLocation() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        Timber.d("getLastLocation called");
+
+        FusedLocationProviderClient fusedLocationClient = LocationServices
+                .getFusedLocationProviderClient(getActivity());
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(getActivity(), location -> {
                     if(location != null) {
-                        userLocation = location;
-                        setupUI();
+                        setupViewModel(location);
                     }
                 });
     }
 
-    private void setupUI() {
-        setupAppBar();
-        setupViewModel();
-    }
+    private void setupViewModel(Location location) {
+        TrailDetailViewModelFactory factory = InjectorUtils.provideTrailDetailViewModelFactory(
+                getActivity().getApplicationContext(), trailId, location);
 
-    private void setupViewModel() {
-        factory = InjectorUtils.provideTrailDetailViewModelFactory(
-                getActivity().getApplicationContext(), trailId, userLocation);
-
-        viewModel = new ViewModelProvider(this, factory).get(TrailDetailViewModel.class);
+        TrailDetailViewModel viewModel = new ViewModelProvider(this, factory)
+                .get(TrailDetailViewModel.class);
 
         viewModel.getTrail().observe(getViewLifecycleOwner(), trail -> {
             if(trail != null) {
-                currentTrail = trail;
-                binding.setTrail(currentTrail);
-                setupMap();
+                binding.setTrail(trail);
+                addTrailMarker();
                 Timber.d("Non-null trail has been returned by LiveData");
             }
             Timber.d("Updating trail from LiveData in ViewModel");
@@ -133,11 +116,20 @@ public class TrailDetailFragment extends Fragment implements OnMapReadyCallback 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Timber.d("onMapReady called");
         googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         map = googleMap;
+        map.setMyLocationEnabled(true);
 
-        double lat = currentTrail.getLatitude();
-        double lon = currentTrail.getLongitude();
+        // Disable scrollview when touch event is over map
+        ((MyMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment_container))
+                .setOnTouchListener(() ->
+                        nestedScrollView.requestDisallowInterceptTouchEvent(true));
+    }
+
+    private void addTrailMarker() {
+        double lat = binding.getTrail().getLatitude();
+        double lon = binding.getTrail().getLongitude();
         float zoom = 10;
         LatLng trailLocation = new LatLng(lat,lon);
         String snippet = String.format(Locale.getDefault(),
@@ -146,37 +138,24 @@ public class TrailDetailFragment extends Fragment implements OnMapReadyCallback 
 
         map.addMarker(new MarkerOptions()
                 .position(trailLocation)
-                .title(currentTrail.getName())
+                .title(binding.getTrail().getName())
                 .snippet(snippet));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(trailLocation,zoom));
-        map.setMyLocationEnabled(true);
-
-        // Disable scrollview when touch event is over map
-        ((MyMapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment_container))
-                .setOnTouchListener(() -> nestedScrollView.requestDisallowInterceptTouchEvent(true));
     }
 
-    private void setupAppBar() {
-        appBar = binding.appBar;
-        nestedScrollView = binding.trailDetailScrollingLayout.nestedScrollView;
-        rootLayout = (ViewGroup) binding.getRoot();
-
+    private void enableToolbarLock() {
+        AppBarLayout appBar = binding.appBar;
         appBar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
-
             if(canDisableScrollView()) disableScrollViewWhenNeeded(appBarLayout, verticalOffset);
-
         });
     }
 
     private boolean canDisableScrollView() {
         int scrollViewHeight = nestedScrollView.getHeight();
-        int rootLayoutHeight = rootLayout.getHeight();
+        int rootLayoutHeight = binding.getRoot().getHeight();
         // If the entire scrollview's content can fit on the screen after the toolbar is collapsed
         // then it is safe to disable scrolling
-        if(scrollViewHeight < rootLayoutHeight) {
-            return true;
-        }
-        return false;
+        return scrollViewHeight < rootLayoutHeight;
     }
 
     private void disableScrollViewWhenNeeded(AppBarLayout appBarLayout, int verticalOffset) {
